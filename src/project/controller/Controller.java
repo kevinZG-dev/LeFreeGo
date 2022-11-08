7a4cacd05d3ca40caf05c702db75af5cfaab984d
Sherlaut <charlottebergot1@gmail.com>
2022-11-08 03:25:56 +0100

    add JFrame

--- src/project/controller/Controller.java
+++ src/project/controller/Controller.java
@@ -1,9 +1,15 @@
 package project.controller;
 
+import javax.swing.*;
+
 public class Controller {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame("Le FreeGo");
+		frame.pack();
+		frame.setVisible(true);
 
 	}
 
