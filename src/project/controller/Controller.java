e5f4585c3d8433fb1ef4c60aed98615c505da3fd
Maumoe <116779075+Maumoe@users.noreply.github.com>
2022-11-08 04:30:13 +0100

    add panel

--- src/project/controller/Controller.java
+++ src/project/controller/Controller.java
@@ -1,6 +1,8 @@
 package project.controller;
 
+import java.awt.*;
 import javax.swing.*;
+
 
 public class Controller {
 
@@ -8,6 +10,11 @@ public class Controller {
 		// TODO Auto-generated method stub
 		
 		JFrame frame = new JFrame("Le FreeGo");
+
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(450,300));
+		frame.add(panel);
+
 		frame.pack();
 		frame.setVisible(true);
 
